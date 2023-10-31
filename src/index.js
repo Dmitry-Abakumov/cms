60d0d761b7c083c5e2c4836f2cfb370bd17410bd
Dmitry Abakumov <dm.abakumov@gmail.com>
2023-10-31 12:45:04 +0200

    error fix

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import App from "./App";
+
 import pageInformation from "./page-information.json";
 
 import "./index.css";
-import App from "./App";
 
-const baseName = `/${pageInformation.find(({ order }) => order === 0).path}`;
+const baseName = `/${pageInformation.find(({ order }) => order === 0).id}`;
+
+console.log(baseName);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={baseName}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
