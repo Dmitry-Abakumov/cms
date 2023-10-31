3e03a5bf7418f2d9df9eb1d8fc36fe317aa4f430
Dmitry Abakumov <dm.abakumov@gmail.com>
2023-10-31 12:51:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-import pageInformation from "./page-information.json";
+// import pageInformation from "./page-information.json";
 
 import "./index.css";
 
-const baseName = `/${pageInformation.find(({ order }) => order === 0).id}`;
+// const baseName = `/${pageInformation.find(({ order }) => order === 0).id}`;
 
-console.log(baseName);
+// console.log(baseName);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={baseName}>
+    <BrowserRouter basename="/cms">
       <App />
     </BrowserRouter>
   </React.StrictMode>
